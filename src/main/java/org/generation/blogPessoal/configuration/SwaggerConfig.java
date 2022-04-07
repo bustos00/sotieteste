bab8003756c8237ec631e59d9f9b31d580561d8f
Thiago Faccipieri <thiago.faccipieri@gmail.com>
2022-04-07 12:29:36 -0300

    test

---
+++ src/main/java/org/generation/blogPessoal/configuration/SwaggerConfig.java
@@ -0,0 +1,56 @@
+package org.generation.blogPessoal.configuration;
+
+import org.springdoc.core.customizers.OpenApiCustomiser;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.responses.ApiResponse;
+import io.swagger.v3.oas.models.responses.ApiResponses;
+
+@Configuration
+public class SwaggerConfig {
+	
+	@Bean
+	public OpenAPI springOpenAPI() {
+		return new OpenAPI()
+				.info(new Info()
+						.title("Blog Pessoal")
+						.description("Blog Pessoal Generation")
+						.version("v0.0.1")
+						.license(new License()
+								.name("Gees Brazil")
+								.url("<https://brazil.generation.org/>"))
+						.contact(new Contact()
+								.name("Bustos Sotie")
+								.url("<https://github.com/bustos00/blog-pessoal-GenerationBR>")
+								.email("javacursobustos@gmail.com")))
+				.externalDocs(new ExternalDocumentation()
+						.description("Github Project")
+						.url("<https://github.com/bustos00/blog-pessoal-GenerationBR>"));
+	}
+
+	private ApiResponse createApiResponse(String message) {
+		return new ApiResponse().description(message);
+	}
+
+	@Bean
+	public OpenApiCustomiser customerGlobalResponseStatus() {
+		return openApi -> {
+			openApi.getPaths().values().forEach(pathItem -> pathItem.readOperations().forEach(operation -> {
+				ApiResponses api = operation.getResponses();
+
+				api.addApiResponse("200", createApiResponse("Sucess!"));
+				api.addApiResponse("201", createApiResponse("Created!"));
+				api.addApiResponse("400", createApiResponse("Request error!"));
+				api.addApiResponse("401", createApiResponse("Not authorized!"));
+				api.addApiResponse("500", createApiResponse("Internal server Error!"));
+			}));
+		};
+	}
+	
+}
